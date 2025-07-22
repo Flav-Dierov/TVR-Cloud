12cd1c52a71729d0530c3e5694b2ff52f6bd4a8b
Flavian Diethelm <flavian.diethelm@protonmail.ch>
2025-07-22 11:19:12 +0200

    Config ergänzt

--- docker/cloud/config.php
+++ docker/cloud/config.php
@@ -1,20 +1,37 @@
 <?php
 $CONFIG = array (
-    'log_rotate_size' => 20 * 1024 * 1024, // 20 MB
-    'trusted_proxies' =>
-    array (
-        0 => '172.0.0.0/8', // To trust all local IPs
-    ),
-    'forwarded_for_headers' =>
-    array (
-        0 => 'HTTP_X_FORWARDED_FOR', // To trust the X-Forwarded-For header
-    ),
-    'htaccess.RewriteBase' => '/', // Needed to prettify URLs
-    'htaccess.IgnoreFrontController' => true, // Needed to prettify URLs
-    'onlyoffice' => 
-    array (
-        'DocumentServerUrl' => '/docs/',
-        'DocumentServerInternalUrl' => 'http://office/',
-        'StorageUrl' => 'http://webserver/',
-    ),
+  'allow_user_to_change_display_name' => false,
+  'allowed_admin_ranges' => [],
+  'debug' => false,
+  'default_language' => 'de',
+  'default_locale' => 'de_CH',
+  'default_phone_region' => 'CH',
+  'default_timezone' => 'Europe/Zurich',
+  'defaultapp' => 'dashboard,files',
+  'enforce_theme' => 'dark',
+  'forbidden_filename_characters' => ['?', '<', '>', ':', '*', '|', '"'], // for Windows compatibility
+  'forwarded_for_headers' => 
+  array (
+    0 => 'HTTP_X_FORWARDED_FOR',
+  ),
+  'htaccess.RewriteBase' => '/',
+  'htaccess.IgnoreFrontController' => true,
+  'log_rotate_size' => 20 * 1024 * 1024, // 20MB for compatibility with VS Code
+  'logtimezone' => 'Europe/Zurich',
+  'maintenance' => false,
+  'maintenance_window_start' => 0,
+  'onlyoffice' => 
+  array (
+    'DocumentServerUrl' => '/docs/',
+    'DocumentServerInternalUrl' => 'http://office/',
+    'StorageUrl' => 'http://webserver/',
+  ),
+  'preview_max_x' => 1024,
+  'preview_max_y' => 1024,
+  'reduce_to_languages' => ['de', 'en', 'fr', 'it'],
+  'trusted_proxies' => 
+  array (
+    0 => '172.0.0.0/8',
+  ),
+  'upgrade.disable-web' => true,
 );
